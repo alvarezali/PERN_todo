c7677e84d6b36c69bf7fc6f42d11bf7e4063f831
Ali Alvarez <alvarezali@hotmail.com>
2026-03-12 17:34:01 -0500

    Routes and server changes 03/12/2026

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -4,18 +4,65 @@ import pool from '../db.js'
 const router = Router();
 
 //Create a new todo
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const {description, completed} = req.body;
         const newTodo = await pool.query(
-            "INSERT INTO todo (description, completed) VALUES ($1, $2) RETURNING *",
+            'INSERT INTO todo (description, completed) VALUES ($1, $2) RETURNING *',
             [description, completed || false]);
         res.json(newTodo.rows[0]);
 
     } catch(err) {
         console.error(err.message);
-        res.status(500).send("Server Error");
+        res.status(500).send('Server Error');
     }
 });
+
+//Get all todos
+router.get('/', async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+//Update a todo
+router.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {description, completed} = req.body;
+        const updatedTodo = await pool.query(
+        "UPDATE todo SET description = $1, completed = $2 WHERE todo_id = $3 RETURNING *",
+        [description, completed, id]
+        );
+        res.json({
+            message: 'Todo was updated',
+            todo: updatedTodo.rows[0]
+        });
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+//Delete a todo
+router.delete('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+        res.json(`Todo id number ${id} was deleted`);
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+});
+
+
+
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/todos", todoRoutes);
+app.use('/todos', todoRoutes);
 
 
 app.listen(5000, () => {
-    console.log("Server is listening on port 5000");
+    console.log('Server is listening on port 5000');
 });
