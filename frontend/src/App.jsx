60601a002b7a2f6845b36a86db21c6eac5f17a17
Ali Alvarez <alvarezali@hotmail.com>
2026-03-18 17:56:10 -0500

    App.jsx form update 03/18/2026

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
 import {useState} from 'react'
+import axios from 'axios';
+import {URL} from './config.js'
 
 function App() {
 
-  const [description, setDescription] = useState('');
   const [todos, setTodos] = useState([]);
   const [editTodo, setEditTodo] = useState(null);
   const [editedText, setEditedText] = useState('');
+  
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target)
+    const dataObj = Object.fromEntries(formData.entries());
+    
+    try {
+      await axios.post(`${URL}/todos`, {
+        ...dataObj,
+        completed: false,
+      });
+
+    } catch(err) {
+      console.error(err.message);
+    }
+  }
 
   return (
 
@@ -14,18 +31,17 @@ function App() {
         
         <h1 className="text-4xl font-bold text-gray-700 mb-8">PERN TODO APP</h1>
         
-        <form className='flex items-center gap-2 shadow-sm border p-2 rounded-lg mb-6'>
+        <form onSubmit={onSubmitForm} className='flex items-center gap-2 shadow-sm border p-2 rounded-lg mb-6'>
           <input 
             className='flex-1 w-full outline-none px-3 py-2 text-gray-600 placeholder-gray-600 '
-            type="text" value={description} 
-            onChange={(e) => setDescription(e.target.value)} 
+            name='description'
+            type="text"  
             placeholder='Your task here' 
             required 
           />
           <button className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md font-medium cursor-pointer'>
             Add task
           </button>
-
         </form>
       </div>
     </div>
